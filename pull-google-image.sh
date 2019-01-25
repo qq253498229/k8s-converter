05fa82356cfd52955cb81556523d1dbacbe19f90
大胃王 <253498229@qq.com>
2019-01-25 10:09:06 +0800

    update to kubernetes 1.13.2

--- pull-google-image.sh
+++ pull-google-image.sh
@@ -1,28 +1,40 @@
 #!/usr/bin/env bash
-docker pull codeforfun/kube-proxy-amd64:v1.11.1
-docker tag codeforfun/kube-proxy-amd64:v1.11.1 k8s.gcr.io/kube-proxy-amd64:v1.11.1
-docker rmi codeforfun/kube-proxy-amd64:v1.11.1
-docker pull codeforfun/kube-controller-manager-amd64:v1.11.1
-docker tag codeforfun/kube-controller-manager-amd64:v1.11.1 k8s.gcr.io/kube-apiserver-amd64:v1.11.1
-docker rmi codeforfun/kube-controller-manager-amd64:v1.11.1
-docker pull codeforfun/kube-scheduler-amd64:v1.11.1
-docker tag codeforfun/kube-scheduler-amd64:v1.11.1 k8s.gcr.io/kube-controller-manager-amd64:v1.11.1
-docker rmi codeforfun/kube-scheduler-amd64:v1.11.1
-docker pull codeforfun/kube-apiserver-amd64:v1.11.1
-docker tag codeforfun/kube-apiserver-amd64:v1.11.1 k8s.gcr.io/kube-scheduler-amd64:v1.11.1
-docker rmi codeforfun/kube-apiserver-amd64:v1.11.1
-docker pull codeforfun/coredns:1.1.3
-docker tag codeforfun/coredns:1.1.3 k8s.gcr.io/coredns:1.1.3
-docker rmi codeforfun/coredns:1.1.3
-docker pull codeforfun/etcd-amd64:3.2.18
-docker tag codeforfun/etcd-amd64:3.2.18 k8s.gcr.io/etcd-amd64:3.2.18
-docker rmi codeforfun/etcd-amd64:3.2.18
-docker pull codeforfun/flannel:v0.10.0-amd64
-docker tag codeforfun/flannel:v0.10.0-amd64 quay.io/coreos/flannel:v0.10.0-amd64
-docker rmi codeforfun/flannel:v0.10.0-amd64
-docker pull codeforfun/pause:3.1
-docker tag codeforfun/pause:3.1 k8s.gcr.io/pause:3.1
-docker rmi codeforfun/pause:3.1
+
+# k8s image
+
+docker pull registry.cn-qingdao.aliyuncs.com/wangdali/tiller:v2.12.3
+docker tag registry.cn-qingdao.aliyuncs.com/wangdali/tiller:v2.12.3 gcr.io/kubernetes-helm/tiller:v2.12.3
+docker rmi registry.cn-qingdao.aliyuncs.com/wangdali/tiller:v2.12.3
+docker pull registry.cn-qingdao.aliyuncs.com/wangdali/kube-proxy:v1.13.2
+docker tag registry.cn-qingdao.aliyuncs.com/wangdali/kube-proxy:v1.13.2 k8s.gcr.io/kube-proxy:v1.13.2
+docker rmi registry.cn-qingdao.aliyuncs.com/wangdali/kube-proxy:v1.13.2
+docker pull registry.cn-qingdao.aliyuncs.com/wangdali/kube-apiserver:v1.13.2
+docker tag registry.cn-qingdao.aliyuncs.com/wangdali/kube-apiserver:v1.13.2 k8s.gcr.io/kube-apiserver:v1.13.2
+docker rmi registry.cn-qingdao.aliyuncs.com/wangdali/kube-apiserver:v1.13.2
+docker pull registry.cn-qingdao.aliyuncs.com/wangdali/kube-controller-manager:v1.13.2
+docker tag registry.cn-qingdao.aliyuncs.com/wangdali/kube-controller-manager:v1.13.2 k8s.gcr.io/kube-controller-manager:v1.13.2
+docker rmi registry.cn-qingdao.aliyuncs.com/wangdali/kube-controller-manager:v1.13.2
+docker pull registry.cn-qingdao.aliyuncs.com/wangdali/kube-scheduler:v1.13.2
+docker tag registry.cn-qingdao.aliyuncs.com/wangdali/kube-scheduler:v1.13.2 k8s.gcr.io/kube-scheduler:v1.13.2
+docker rmi registry.cn-qingdao.aliyuncs.com/wangdali/kube-scheduler:v1.13.2
+docker pull registry.cn-qingdao.aliyuncs.com/wangdali/nginx-ingress-controller:0.21.0
+docker tag registry.cn-qingdao.aliyuncs.com/wangdali/nginx-ingress-controller:0.21.0 quay.io/kubernetes-ingress-controller/nginx-ingress-controller:0.21.0
+docker rmi registry.cn-qingdao.aliyuncs.com/wangdali/nginx-ingress-controller:0.21.0
+docker pull registry.cn-qingdao.aliyuncs.com/wangdali/coredns:1.2.6
+docker tag registry.cn-qingdao.aliyuncs.com/wangdali/coredns:1.2.6 k8s.gcr.io/coredns:1.2.6
+docker rmi registry.cn-qingdao.aliyuncs.com/wangdali/coredns:1.2.6
+docker pull registry.cn-qingdao.aliyuncs.com/wangdali/etcd:3.2.24
+docker tag registry.cn-qingdao.aliyuncs.com/wangdali/etcd:3.2.24 k8s.gcr.io/etcd:3.2.24
+docker rmi registry.cn-qingdao.aliyuncs.com/wangdali/etcd:3.2.24
+docker pull registry.cn-qingdao.aliyuncs.com/wangdali/flannel:v0.10.0-amd64
+docker tag registry.cn-qingdao.aliyuncs.com/wangdali/flannel:v0.10.0-amd64 quay.io/coreos/flannel:v0.10.0-amd64
+docker rmi registry.cn-qingdao.aliyuncs.com/wangdali/flannel:v0.10.0-amd64
+docker pull registry.cn-qingdao.aliyuncs.com/wangdali/pause:3.1
+docker tag registry.cn-qingdao.aliyuncs.com/wangdali/pause:3.1 k8s.gcr.io/pause:3.1
+docker rmi registry.cn-qingdao.aliyuncs.com/wangdali/pause:3.1
+docker pull registry.cn-qingdao.aliyuncs.com/wangdali/defaultbackend:1.4
+docker tag registry.cn-qingdao.aliyuncs.com/wangdali/defaultbackend:1.4 k8s.gcr.io/defaultbackend:1.4
+docker rmi registry.cn-qingdao.aliyuncs.com/wangdali/defaultbackend:1.4
 
 # ingress addons
 docker pull codeforfun/defaultbackend:1.4
